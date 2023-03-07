af912fef3da6d41add81977b1eda84b0c84c0c2f
Paul <borovkov-pavel@yandex.ru>
2023-03-07 21:22:35 +0300

    Edited problems. Half alpha release version modified:   script.py

--- script.py
+++ script.py
@@ -5,14 +5,18 @@ import dask.dataframe as dd
 import time
 import dask
 from dask.distributed import Client
+from dask.diagnostics import ProgressBar
+from dask.distributed import progress
+
 import multiprocessing
 import os
 
 #defines
-PATH = "./files"
+PATH = "M:/Fast_Work/files/"
 directory = os.fsencode(PATH)
 NCORES = 2
 NWORKERS = 4
+DB = 1
 
         #cmd line module init
 cmd_line = argparse.ArgumentParser(description='Script for parsing and saving omic data')
@@ -21,7 +25,7 @@ cmd_line.add_argument(
     '--id',
     '-i',
     type=str,
-    default=None,
+    default="SRX8026866",
     help='Experimental ID'
 )
 
@@ -82,25 +86,30 @@ def get_matching_experimnet_part(
     # options - list of arguments
     part = df.partitions[num]
     
-    if options[0] != '':
-        tmp = options[0].split(',')
-        part = part.loc[part['id'].isin(tmp)]
-    if options[1] != '':
-        tmp = options[1].split(',')
-        part = part.loc[part['Genome assembly'].isin(tmp)]
-    if options[2] != '':
-        tmp = options[2].split(',')
-        part = part.loc[part['Antigen class'].isin(tmp)]
-    if options[3] != '':
-        tmp = options[3].split(',')
-        part = part.loc[part['Antigen'].isin(tmp)]
-    if options[4] != '':
-        tmp = options[4].split(',')
-        part = part.loc[part['Cell type class'].isin(tmp)]
-    if options[5] != '':
-        tmp = options[5].split(',')
-        part = part.loc[part['Cell type'].isin(tmp)]
-    
+    for key in options.keys():
+        if options[key]:
+            tmp = options[key].split(',')
+            part = part.loc[part[key].isin(tmp)]
+
+    # if options.id != '':
+    #     tmp = options.id.split(',')
+    #     part = part.loc[part['id'].isin(tmp)]
+    # if options[1] != '':
+    #     tmp = options[1].split(',')
+    #     part = part.loc[part['Genome assembly'].isin(tmp)]
+    # if options[2] != '':
+    #     tmp = options[2].split(',')
+    #     part = part.loc[part['Antigen class'].isin(tmp)]
+    # if options[3] != '':
+    #     tmp = options[3].split(',')
+    #     part = part.loc[part['Antigen'].isin(tmp)]
+    # if options[4] != '':
+    #     tmp = options[4].split(',')
+    #     part = part.loc[part['Cell type class'].isin(tmp)]
+    # if options[5] != '':
+    #     tmp = options[5].split(',')
+    #     part = part.loc[part['Cell type'].isin(tmp)]
+
     part = part.compute()
     return list(part.id)
 
@@ -111,13 +120,15 @@ def create_matching_expirement_list(
             options
         ): #    Return expirement names list
     
+    
     process_list = []
     matching_experiments = []
     
     df = dd.read_csv(
-                    filename,
-                    sep = ',', 
+                    PATH + filename,
+                    sep = '\t', 
                     names = ['id', 'Genome assembly', 'Antigen class', 'Antigen', 'Cell type class', 'Cell type'],
+                    usecols=range(6),
                     blocksize = '10mb'
                 )
     #TODO Можно сделать мапом. Будет красивше
@@ -126,7 +137,8 @@ def create_matching_expirement_list(
     #         range(df.npartitions), 
     #         [options] * df.npartitions
     #     )
-    
+    print("Find file " +  PATH + filename)
+
     for part in range(df.npartitions):
        process_list.append(que.submit(
                                 get_matching_experimnet_part, 
@@ -134,18 +146,18 @@ def create_matching_expirement_list(
                                 part, 
                                 options
                                 ))
-    
+
     for process in process_list:
         matching_experiments.extend(process.result())
-
+    
     return matching_experiments
 
-def add_sorted_bed_2_file(
+def add_sorted_bed_2_file( 
             filename,
             df,
             num,
             matching_experiments
-        ):
+        ): 
     part = df.partitions[num]
 
     part = part.loc[part['id'].isin(matching_experiments)]
@@ -168,7 +180,7 @@ def create_sorted_bed_file(
                 names = ["chr", 'begin', 'end', 'id', 'score'],
                 blocksize = '100mb'
                 )
-    
+
     open(path_2_sorted_file, mode = 'w').close()  # Creating empty .csv for editing
     
     for part in range(df.npartitions):
@@ -180,8 +192,10 @@ def create_sorted_bed_file(
                                 matching_experiments
                                 )) 
     #TODO progress bar
-    [process.result() for process in process_list]
-
+    print("Your file creating. You can see progress here:\n http://localhost:8787/status")
+    a = [process.result() for process in process_list]
+    progress(a)
+    
 
 
     
@@ -193,15 +207,20 @@ if __name__ == '__main__':
 
     options = {
         #Parse arguments from cmd line to special dict
-        "id"            :   args.id,
-        "assembly"      :   args.assembly,
-        "antigen_class" :   args.antigen_class,
-        "antigen"       :   args.antigen,
-        "cell_type"     :   args.cell_type,
-        "cell"          :   args.cell
+        "id"                :   args.id,
+        "Genome assembly"   :   args.assembly,
+        "Antigen class"     :   args.antigen_class,
+        "Antigen"           :   args.antigen,
+        "Cell type class"   :   args.cell_type,
+        "Cell type'"        :   args.cell
     }
 
-    matching_experiments = create_matching_expirement_list(que, args.file, options)
+    print("Succes parse arguments!")
+
+    matching_experiments = create_matching_expirement_list(que, "part.txt", options)
+    if DB:
+        print(f"Was finded {len(matching_experiments)} results:\n " + str(matching_experiments))
+
     create_sorted_bed_file(que, args.file, matching_experiments)
 
     que.shutdown()
