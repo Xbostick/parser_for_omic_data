8d5e1f24bf0918dbd43586518fa0af415ee20f98
Paul <borovkov-pavel@yandex.ru>
2023-03-14 21:54:45 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -218,8 +218,8 @@ def create_feature(
         path
     ):
     
-    key[0] = key[0].replace(' ', '_')
-    key[1] = key[1].replace(' ', '_')
+    key[0] = str(key[0]).replace(' ', '_')
+    key[1] = str(key[1]).replace(' ', '_')
 
     # chroms - list with chroms of the organism
     chroms = list(sizes.keys())
@@ -240,7 +240,7 @@ def create_feature(
     data_sparse = {chrm:SparseVector(data[chrm]) for chrm in chroms}
     
     os.makedirs(os.path.expanduser(path) +"/omicDC_results", exist_ok=True)
-    dump(data_sparse, path+"/omicDC_results/" + "_".join(key) + ".pkl", 3)
+    dump(data_sparse, path+"/omicDC_results/" + key[1] + "_"+key[0] + ".pkl", 3)
 
 
 def create_features_files(
