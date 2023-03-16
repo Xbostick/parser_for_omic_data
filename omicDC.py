590eda8bce31fff61fcdd811c41ef3983509b50b
Paul <borovkov-pavel@yandex.ru>
2023-03-16 21:22:34 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -142,7 +142,7 @@ def create_matching_expirement_df(
             match_exp_df = match_exp_df.loc[match_exp_df[key].isin(tmp)]
 
     return match_exp_df
-pbar = 0
+
 def add_sorted_bed_2_file( 
             filename,
             df,
@@ -153,9 +153,8 @@ def add_sorted_bed_2_file(
 
     part = part.loc[part['id'].isin(matching_experiments)]
     part = part.compute()
-    pbar +=1
     part.to_csv(filename, index=False, header=False, mode='a')
-    print(pbar + '\r')
+
     return num
 #python3 omicDC.py -g mm9 -b Histone  
 def im_not_alone(filename):
