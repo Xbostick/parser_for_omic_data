d54eee421abec024d125f76a0a944368005d2f4c
Xbostick <borovkov-pavel@yandex.ru>
2023-04-08 22:20:28 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -144,12 +144,10 @@ def check_intersection(row1, row2):
 
 def make_intersect(df,num,filename):
     part = df.partitions[num]
-    print(part.shape)
     part['intersects'] = part.apply(lambda row: check_intersection(row[:5], row[5:]), axis=1)
     part = part.loc[part['intersects'] == True, ['chr', 'begin', 'end', 'id', 'score']]
-    print(part.shape)
     #part.to_csv(filename, index=False, header=False, mode='a')
-    return num
+    return part
 
 
 def add_user_bed_markers(
@@ -183,13 +181,13 @@ def add_user_bed_markers(
     print(df_m.shape)
     print(df_m.npartitions)
     for part in range(df_m.npartitions):
-        process_list.append(que.submit(
-                                make_intersect,
+        process_list.append(make_intersect(
                                 df_m,
                                 part,
                                 path_2_sorted_file_with_user_bed
                                 )) 
-    
+    print(type(process_list[1]))
+    exit()
     a = [process.result() for process in process_list]
 
     return df.compute()
