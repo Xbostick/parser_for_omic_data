24f79eb7fe6c0e62323fa1cdaf77a06edc5be9c0
Xbostick <borovkov-pavel@yandex.ru>
2023-04-08 21:15:28 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -145,7 +145,7 @@ def check_intersection(row1, row2):
 
 def make_intersect(df,num,filename):
     part = df.partitions[num]
-    part['intersects'] = part.apply(lambda row: check_intersection(row[:5], row[5:]), axis=1, meta = part)
+    part['intersects'] = part.apply(lambda row: check_intersection(row[:5], row[5:]), axis=1)
     print(2)
     part = part.loc[part['intersects'] == True, ['chr', 'begin', 'end', 'id', 'score']]
     print(3)
