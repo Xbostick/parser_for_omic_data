4e4e53cf7c773abc3948b4d54b221a128a774b02
Xbostick <borovkov-pavel@yandex.ru>
2023-04-08 18:56:38 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -254,14 +254,14 @@ def create_feature(
     chroms = list(sizes.keys())
     # data - dict with values of exp for each cromosome
     data = {chrm: np.zeros(sizes[chrm], dtype=np.uint16) for chrm in chroms}
-    
+    print(chroms)
     exp_df = exp_df[exp_df['id'].isin(exps)]
     exp_df = exp_df[exp_df['chr'].isin(chroms)]
-    
+    print(chroms)
     for line in exp_df.values:
         chrm, begin, end, ee, value = line
         data[chrm][begin: end] = np.maximum(data[chrm][begin: end], value)
-    print(chroms)
+
     # data_sparse - convert data to sparse
     data_sparse = {chrm:SparseVector(data[chrm]) for chrm in chroms}
     
