c4b18a48a970d19c65aa8780c886605b7fa79e05
Xbostick <110611760+Xbostick@users.noreply.github.com>
2023-04-08 13:12:59 +0300

    Add files via upload

--- omicDC.py
+++ omicDC.py
@@ -171,14 +171,10 @@ def add_sorted_bed_2_file(
                         sep = '\t',
                         names = ['chr', 'begin_b', 'end_b'],
                     )
-        print(f"part {part.head()} bed {bed_file_df.head()}")
         part = part.merge(bed_file_df, on='chr')
-        print("1\n\n\n\n\n\n\n\n\n")
         part['intersects'] = part.apply(lambda row: check_intersection(row[:5], row[5:]), axis=1)
-        print("2\n\n\n\n\n\n\n\n\n")
         part = part.loc[part['intersects'] == True, ['chr', 'begin', 'end', 'id', 'score']]
-        print("3\n\n\n\n\n\n\n\n\n")
-
+        
     part = part.compute()
     part.to_csv(filename, index=False, header=False, mode='a')
     return num
