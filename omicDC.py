421e3d9278b15d2420af982ff626a40f42b4d4b2
MrARVO <voytetskiyartem@gmail.com>
2023-04-13 20:58:28 +0300

    modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -155,6 +155,7 @@ def add_sorted_bed_2_file(
     part.to_csv(filename, index=False, header=False, mode='a')
     return num
 
+
 def im_not_alone(filename):
     """Function to check if only one user making executions"""
     directory = os.listdir('./')
@@ -164,12 +165,14 @@ def im_not_alone(filename):
             is_multiuser = 1
     return is_multiuser
 
+
 def make_intersect(df,num):
     part = df.partitions[num]
     df['intersects'] = df.apply(lambda row: check_intersection(row[:5], row[5:]), axis=1)
     df = df.loc[df['intersects'] == True, ['chr', 'begin', 'end', 'id', 'score']]
     df = df.compute()
     return num
+
 
 def add_user_bed_markers(
         que,
@@ -252,7 +255,9 @@ def create_feature(
     ):
     """Creating features"""
 
+    print('i am here')
     print(exp_df[3])
+    print('i am not here')
 
     # chroms - list with chroms of the organism
     chroms = list(sizes.keys())
