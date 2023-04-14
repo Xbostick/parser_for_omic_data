708bb98d05161821542d9a514c0701c35cb0518f
Paul <borovkov-pavel@yandex.ru>
2023-04-14 13:35:52 +0300

    Hardcode params
    	modified:   omicDC.py

--- omicDC.py
+++ omicDC.py
@@ -254,7 +254,7 @@ def create_features_files(
         names = ['chr', 'begin', 'end', 'id', 'score']
         )
     
-    Parallel(n_jobs=int(NCORES))(delayed(create_feature)(key, list(loc_df['id']), sizes, exp_df, path) 
+    Parallel(n_jobs=int(NCORES+4))(delayed(create_feature)(key, list(loc_df['id']), sizes, exp_df, path) 
                    for key, loc_df in match_exp_df.groupby(['Antigen class', 'Antigen']))
 
 
